c68b86893fd32dc870b906216595adc7b47e2cec
Amuthan Mannar <amuthanmannar4@gmail.com>
2019-07-13 18:59:48 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,15 +5,28 @@
 #include "DynamicList.h"
 #include "HashMap.h"
 #include "Comparator.h"
+#include "PriorityQueue.h"
 
 struct amuthan{
 	int roll;
 	char name[10];
 };
+int compare(void* one,void* two){
+	struct amuthan *op1,*op2;
+	op1=(struct amuthan*) one;
+	op2=(struct amuthan*) two;
 
+  if(op1->roll==op2->roll){
+      return 0;
+  }
+  if(op1->roll>op2->roll){
+      return 1;
+  }
+  if(op1->roll<op2->roll){
+      return -1;
+  }
+}
 void main(){
-	
-	//creation of our own data types/ objects
 	struct amuthan a,b,c,d,e,f;
 	strcpy(a.name,"amuthan");
 	strcpy(b.name,"siva");
@@ -23,8 +36,8 @@ void main(){
 	strcpy(f.name,"kkk");
 	a.roll=10;b.roll=20;c.roll=30;d.roll=40;e.roll=50;f.roll=60;
 
-	//Linked list - demo
 
+	/*
 	LinkedList *list;
 	list=linkedlist();
 	LLadd(list,&a);
@@ -43,9 +56,9 @@ void main(){
     while(hasNext(i)){
     	printf("%s ",((struct amuthan*)getNext(i))->name);
     }
+*/
 
-        //stack -demo
-
+/*
 	Stack *st;
 	st=stack();
 	push(st,&a);
@@ -62,7 +75,8 @@ void main(){
 
 
 
-	// arraylist-demo
+	/*
+
  ArrayList* al;
  al=arrayList();
 
@@ -93,10 +107,22 @@ void main(){
  	printf("%s ",((struct amuthan*)getNext(i))->name);
  }
 
- */
+ Comparator* comp;
+ comp=getComparatorAL(compare,al);
+ sortAsc(comp);
 
 
- //hashmap -demo
+ printf("after sorting.. \n");
+ Iterator* i1;
+ i1=ALgetIterator(al);
+
+
+ while(hasNext(i1)){
+ 	printf("%s ",((struct amuthan*)getNext(i1))->name);
+ }
+*/
+
+/*
 HashMap* map;
 map=hashmap();
 put(map,"am",&a);
@@ -109,6 +135,51 @@ data=getValue(map,"sarkar");
 printf(" data is %d",((struct amuthan*)data)->roll);
 */
 
+
+	/*
+LinkedList *list1;
+list1=linkedlist();
+LLadd(list1,&b);
+LLadd(list1,&d);
+LLadd(list1,&c);
+
+Iterator* i1;
+i1=LLgetIterator(list1);
+
+Comparator* comp;
+comp=getComparatorLL(compare,list1);
+sortDesc(comp);
+
+
+printf("after sorting.. \n");
+while(hasNext(i1)){
+	printf("%s ",((struct amuthan*)getNext(i1))->name);
+}
+*/
+	
+	/*
+
+	PriorityQueue *q;
+	q=priorityQueue(compare);
+	enqueue(q,&d);
+	enqueue(q,&b);
+	enqueue(q,&c);
+	enqueue(q,&a);
+	enqueue(q,&e);
+	dequeue(q);
+    struct amuthan *got;
+    got=(struct amuthan*)dequeue(q);
+    printf("got.. %s \n",got->name);
+	Iterator* i;
+	i=PQgetIterator(q);
+	printf("after sorting.. \n");
+	while(hasNext(i)){
+		printf("%s ",((struct amuthan*)getNext(i))->name);
+	}
+
+
+
+*/
 
 
 
